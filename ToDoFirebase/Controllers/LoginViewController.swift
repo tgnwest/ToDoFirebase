3ae952767961ebaa44fe8205c953391b8cb250a0
Alex <tgnwest@gmail.com>
2020-05-19 16:07:21 +0300

    added firebase

---
+++ ToDoFirebase/Controllers/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  ToDoFirebase
+//
+//  Created by Alex Tregubov on 18.05.2020.
+//  Copyright © 2020 Alex Tregubov. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var warnLabel: UILabel!
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+    }
+    @IBAction func registerTapped(_ sender: UIButton) {
+    }
+    
+}
+
---
+++ ToDoFirebase/Controllers/TasksViewController.swift
@@ -0,0 +1,34 @@
+//
+//  TasksViewController.swift
+//  ToDoFirebase
+//
+//  Created by Alex Tregubov on 19.05.2020.
+//  Copyright © 2020 Alex Tregubov. All rights reserved.
+//
+
+import UIKit
+
+class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.backgroundColor = .clear
+        cell.textLabel?.text = "This is cell number \(indexPath.row)"
+        cell.textLabel?.textColor = .white
+        return cell
+    }
+
+    @IBAction func addTapped(_ sender: UIBarButtonItem) {
+    }
+}
